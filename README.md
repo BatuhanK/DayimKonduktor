e70a556511a4858e3a3e86956be857b946d909fd
Batuhan KATIRCI <katircib@gmail.com>
2018-01-16 07:03:59 +0300

    aa tren mi o?

---
+++ README.md
@@ -0,0 +1,14 @@
+# DayimKonduktor
+Dogu ekspresinde yatakli vagon icin toplu olarak yer sorgulamaya yarayan arac
+
+[![onizleme](https://asciinema.org/a/COiPj6eaVVMMVD7Qj9kJsGnfp.png)](https://asciinema.org/a/COiPj6eaVVMMVD7Qj9kJsGnfp)
+
+```bash
+
+## install dependencies
+npm install
+
+## run with DAY env variable (optional, default is 30)
+DAYS=30 npm start
+
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+const moment = require('moment');
+const ProgressBar = require('ascii-progress');
+const Table = require('cli-table');
+const PromisePool = require('es6-promise-pool');
+
+const { getRemainingSeats } = require('./lib/api');
+const DAYS = Number(process.env.DAYS) || 30;
+
+const generatePromises = function* () {
+  for (let i = 0; i <= DAYS; i++) {
+    const date = moment().add(i, 'days');
+    yield Promise.all([
+      getRemainingSeats('Ankara Gar', 'Kars', date),
+      getRemainingSeats('Kars', 'Ankara Gar', date)
+    ]);
+  }
+};
+
+const bar = new ProgressBar({
+  schema: 'Günler sorgulanıyor(:current/:total) :bar (kalan süre: :elapsed)',
+  total: DAYS+1
+});
+const table = new Table({ head: ['', 'Ankara => Kars', 'Kars => Ankara']});
+
+const promiseIterator = generatePromises();
+const pool = new PromisePool(promiseIterator, 5);
+
+pool.addEventListener('fulfilled', event => {
+  bar.tick();
+  if (bar.completed) {
+    bar.clear();
+  }
+  const result = event.data.result;
+  const ankaraKars = result.find(r => r.to == 'Kars');
+  const karsAnkara = result.find(r => r.from == 'Kars');
+  table.push({ [ankaraKars.date]: [ankaraKars.remainingSeats, karsAnkara.remainingSeats] });
+});
+
+pool
+  .start()
+  .then(() => {
+    console.log(table.toString()); //eslint-disable-line
+  });
