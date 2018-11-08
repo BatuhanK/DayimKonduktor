14b903ce0f723bef67f9454b1e3c363e30942edb
Batuhan KATIRCI <katircib@gmail.com>
2018-11-08 04:03:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ProgressBar = require('ascii-progress');
 const Table = require('cli-table');
 const PromisePool = require('es6-promise-pool');
 
-const { getRemainingSeats } = require('./lib/api');
+const { getRemainingSeats } = require('./lib/api')
 const DAYS = Number(process.env.DAYS) || 32;
 
 const generatePromises = function* () {
